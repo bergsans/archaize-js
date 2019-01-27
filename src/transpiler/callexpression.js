180c52ff2a8b857c16309a2c1e1495b888e77202
claes-magnus <cmbernson@gmail.com>
2019-01-27 15:42:18 +0100

    Remove zero index of transpile funcs (bug)

--- src/transpiler/callexpression.js
+++ src/transpiler/callexpression.js
@@ -4,24 +4,28 @@ function replaceCallExpression(node) {
    
   if (node.arguments.includes('ArrowFunctionExpression')) {
 
-    if (node.arguments[0].body.type !== 'BlockStatement') {
-     
-      let oldBody = { ...node.arguments[0].body };
-      let newBody = { 
-        type: 'BlockStatement',
-        body: [
-          {
-            type: 'ReturnStatement',
-            argument: { ...oldBody }
-          } 
-        ]
-      };
-      node.arguments[0].expression = false;
-      node.arguments[0].body = newBody;
-    } else {
-      node.arguments[0].body.type = 'BlockStatement';
-    }  
-  node.arguments[0].type = 'FunctionExpression';
+    node.arguments.forEach((arg) => {    
+      if (arg.body.type !== 'BlockStatement') {
+       
+        let oldBody = { ...arg.body };
+        let newBody = { 
+          type: 'BlockStatement',
+          body: [
+            {
+              type: 'ReturnStatement',
+              argument: { ...oldBody }
+            } 
+          ]
+        };
+        arg.expression = false;
+        arg.body = newBody;
+      } else {
+        arg.body.type = 'BlockStatement';
+      }  
+    arg.type = 'FunctionExpression';
+
+  });
+
   } 
 
   node.arguments.forEach((arg) => {
--- src/transpiler/variabledeclaration.js
+++ src/transpiler/variabledeclaration.js
@@ -1,53 +1,49 @@
-/*
-TODO: Check if polyfill is already added to file.
+const isMethodES6 = (node, mName, i) => (node.declarations[i] 
+     && node.declarations[i].init 
+     && node.declarations[i].init.callee 
+     && node.declarations[i].init.callee.property 
+     && node.declarations[i].init.callee.property.name
+     && node.declarations[i].init.callee.property.name === mName);
 
-*/
-const isMethodES6 = (node, mName) => (node.declarations[0] 
-     && node.declarations[0].init 
-     && node.declarations[0].init.callee 
-     && node.declarations[0].init.callee.property 
-     && node.declarations[0].init.callee.property.name
-     && node.declarations[0].init.callee.property.name === mName);
- 
 const isVariableDeclaration = (node) => (node.type === 'VariableDeclaration');
 
 let namesIds = [];
 
 function replaceVariableDeclarations(node) {
-
-  let idName = node.declarations[0].id.name;
-  namesIds = [...namesIds, idName];
-
-  if (namesIds.filter((elIdName) => elIdName === idName).length > 1) {
-    const count = namesIds.filter((elIdName) => elIdName === idName).length;
-    node.declarations[0].id.name = `${idName}${count}`;
-  }  
-
-  node.declarations[0].init.type = node.declarations[0].init.type === 'ArrowFunctionExpression'? 
-        'FunctionExpression'
-        : 
-        node.declarations[0].init.type;
-
   let polyfill = undefined;
 
-  if (isMethodES6(node, 'includes')) {
-    polyfill = 'POLYFILL_INCLUDES' 
-  } else if (isMethodES6(node, 'startsWith')) {
-    polyfill = 'POLYFILL_STARTSWITH';
-  } else if (isMethodES6(node, 'endsWith')) {
-    polyfill = 'POLYFILL_ENDSWITH';
-  } else if (isMethodES6(node, 'repeat')) {
-    polyfill = 'POLYFILL_REPEAT';
-  } else if (isMethodES6(node, 'find')) {
-    polyfill = 'POLYFILL_FIND';
-  } else if (isMethodES6(node, 'findIndex')) {
-    polyfill = 'POLYFILL_FIND_INDEX';
-  } 
+  node.declarations.forEach((d, i) => {
+    let idName = d.id.name;
+    namesIds = [...namesIds, idName];
 
+    if (namesIds.filter((elIdName) => elIdName === idName).length > 1) {
+      const count = namesIds.filter((elIdName) => elIdName === idName).length;
+      d.id.name = `${idName}${count}`;
+    }  
+
+    d.init.type = d.init.type === 'ArrowFunctionExpression'? 
+          'FunctionExpression'
+          : 
+          d.init.type;
+
+    if (isMethodES6(node, 'includes', i)) {
+      polyfill = 'POLYFILL_INCLUDES' 
+    } else if (isMethodES6(node, 'startsWith',i)) {
+      polyfill = 'POLYFILL_STARTSWITH';
+    } else if (isMethodES6(node, 'endsWith',i)) {
+      polyfill = 'POLYFILL_ENDSWITH';
+    } else if (isMethodES6(node, 'repeat',i)) {
+      polyfill = 'POLYFILL_REPEAT';
+    } else if (isMethodES6(node, 'find',i)) {
+      polyfill = 'POLYFILL_FIND';
+    } else if (isMethodES6(node, 'findIndex',i)) {
+      polyfill = 'POLYFILL_FIND_INDEX';
+    } 
+  });
   if (polyfill) {
     return [{ ...node, 
       kind: 'var' 
-    },polyfill];
+    }, polyfill];
   } else {
     return { ...node, 
       kind: 'var' 
