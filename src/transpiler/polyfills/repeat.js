63c42f5d04db98f1222790bf586c6add4e9f838e
claes-magnus <cmbernson@gmail.com>
2019-01-27 09:09:17 +0100

    Fix bug

--- src/transpiler/polyfills/repeat.js
+++ src/transpiler/polyfills/repeat.js
@@ -3,24 +3,26 @@
  */
 
 String.prototype.repeat = function(count) { 
+  if (count == Number.POSITIVE_INFINITY) {
+    throw new RangeError('Larger than Infinity.');
+  } else if (count < 0) {
+    throw new RangeError('Negative number');
+  }
+
   var string = this;
   if (typeof count != 'number') {
     throw new TypeError('Not a number');
   }
   var content = string;
   count = parseInt(count, 10);
-  if (count > 0 && count < Infinity) {
+  if (count > 0 && count < Number.POSITIVE_INFINITY) {
     for (var i = 0; i < count; i++) {
       string += content;
     }
     return string;
   } else if (count == 0) {
     return content;
-  } else if (count >= Infinity) {
-    throw new RangeError('Larger than Infinity.');
-  } else if (count < 0) {
-    throw new RangeError('Negative number');
-  }
+  } 
   return undefined;
 }
 module.exports = { String };
