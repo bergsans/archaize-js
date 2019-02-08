25ee8f94f7a5264c1ca15ea166978a3bb2bc4343
claes-magnus <cmbernson@gmail.com>
2019-02-08 12:00:49 +0100

    Begin refactor to fix major bug. string and array includes fixed

--- src/transpiler/ifstatement.js
+++ src/transpiler/ifstatement.js
@@ -1,10 +1,42 @@
+const { traverse } = require('estraverse');
+
 const isIfStatement = (node) => node.type === 'IfStatement';
 
-function replaceIfStatement(node) {
-  if(node.test.type === 'BinaryExpression' && node.test.operator === '===') {
+function returnType(name, ast) {
+  let type = undefined;
+  traverse(ast, {
+    enter: (node) => {
+      if(node.type === 'VariableDeclaration') {
+        if(node.declarations[0].id.name === name) {
+          type = node.declarations[0].init.type;
+        }
+      }
+    }
+  });
+  return type;
+}
+
+function replaceIfStatement(node, ast) {
+
+  let polyfill = undefined;
+
+  if (node.test.type === 'BinaryExpression' && node.test.operator === '===') {
     node.test.operator = '==';
+  } else if (node.test.type === 'CallExpression' && node.test.callee.property.name === 'includes') {
+    
+    let name = node.test.callee.object.name;
+    let type = returnType(name, ast);
+    if (type === 'ArrayExpression') {
+        polyfill = 'POLYFILL_ARR_INCLUDES';
+    } else {
+        polyfill = 'POLYFILL_INCLUDES';
+    }
   }
-  return node;
+  if (polyfill) {
+    return [node, polyfill];
+  } else {
+    return node;
+  }
 }
 module.exports = { isIfStatement, replaceIfStatement };
 
