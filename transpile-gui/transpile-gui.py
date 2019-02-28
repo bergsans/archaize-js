8bf4b86c11fab40ec738a27e040c0fd5d1dfff13
claes-magnus <cmbernson@gmail.com>
2019-02-28 22:07:48 +0100

    Improve code quality

--- transpile-gui/transpile-gui.py
+++ transpile-gui/transpile-gui.py
@@ -4,13 +4,9 @@ from tkinter import filedialog
 from tkinter import *
 import os
 
-
-HEIGHT = 200
-WIDTH = 200
-
 root = Tk()
 
-canvas = Canvas(root, width=WIDTH, height=HEIGHT)
+canvas = Canvas(root, width=200, height=200)
 canvas.pack()
 
 myLabel = Label(root, justify=LEFT, text="Archaize JS")
@@ -18,18 +14,16 @@ myLabel.pack()
 
 def chooseFile():  
   filename = filedialog.askopenfilename(title="Select JS file", filetypes=(("JavaScript files", "*.js"), ("all files","*.*")))
-  if len(filename) > 0:
-    print(filename)
-    file = filename.split("/")
-    this_file = file[len(file) - 1]
-    this_folder = filename[ :len(filename) - len(file)]
-    new_file = "%s/_%s" % (this_folder, this_file)
-    print(this_file)
-    print(this_folder)
-    cm = "../src/archaize.js -i %s -o %s --transpile" % (filename, new_file)
-    os.system(cm)
-  else:
+  if len(filename) == 0:
     print("No file selected")
+  else:
+    print(filename)
+    path = filename.split("/")
+    this_file = path[len(path) - 1]
+    this_folder = filename[ :len(filename) - len(this_file)]
+    new_file = "{0:s}_{1:s}".format(this_folder, this_file)
+    command = "../src/archaize.js -i {0:s} -o {1:s} --transpile".format(filename, new_file)
+    os.system(command)
   
 btn = Button(root, text ="Transpile file", bd=1, relief="ridge", command = chooseFile)
 btn.pack()
